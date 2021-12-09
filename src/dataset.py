25feff2dd5b3ea15784deb89fb5940f5ef80afcf
Wsine <weizhy3@hotmail.com>
2021-12-09 14:50:44 +0800

    refactor dataset

--- src/dataset.py
+++ src/dataset.py
@@ -2,6 +2,7 @@ import math
 import random
 
 import torch
+import torch.utils.data
 import torchvision
 import torchvision.transforms as T
 from sklearn.model_selection import train_test_split
@@ -13,15 +14,11 @@ class RandomApply(object):
     def __init__(self, tran, p=0.5):
         self.tran = tran
         self.prob = p
-        self.apply = False
 
     def __call__(self, x):
         p = random.random()
         if p < self.prob:
-            self.apply = True
             x = self.tran(x)
-        else:
-            self.apply = False
         return x
 
 
@@ -31,20 +28,7 @@ class RandomApplyOne(object):
 
     def __call__(self, x):
         t = random.choice(self.trans)
-        return t(x)
-
-
-class MaskNoiseLabel(object):
-    def __init__(self, persistent=None):
-        self.persistent = persistent
-
-    def set_persistent(self, p):
-        self.persistent = p
-
-    def __call__(self, apply):
-        if self.persistent is not None:
-            return self.persistent
-        return 1 if apply is True else 0
+        return t(x)  # type: ignore
 
 
 class PostTransformDataset(torch.utils.data.Dataset):
@@ -55,104 +39,97 @@ class PostTransformDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         x, y = self.dataset[idx]
-        apply = False
         if self.transform is not None:
             x = self.transform(x)
-            if isinstance(self.transform, T.Compose):
-                for t in self.transform.transforms:
-                    if isinstance(t, RandomApply):
-                        apply = t.apply
         if self.target_transform is not None:
-            y = self.target_transform(apply)
+            y = self.target_transform(y)
         return x, y
 
     def __len__(self):
         return len(self.dataset)
 
 
-@cache_object(filename='dataset.pkl')
-def load_dataset(
-        opt, split,
-        noise=False, noise_type=None,
-        gblur_std=None, target_trsf=False):
-
-    if opt.dataset == 'cifar10':
-        cifar = torchvision.datasets.CIFAR10
-        mean = (0.4914, 0.4822, 0.4465)
-        std = (0.2023, 0.1994, 0.2010)
-    elif opt.dataset == 'cifar100':
-        cifar = torchvision.datasets.CIFAR100
-        mean = (0.5071, 0.4867, 0.4408)
-        std = (0.2675, 0.2565, 0.2761)
+def load_noisy_dataset(_, baseset, comm_trsf, noise_type, gblur_std=None):
+    if noise_type == 'random':
+        trsf = RandomApply(RandomApplyOne([
+            T.GaussianBlur(math.ceil(4*std)//2*2+1, sigma=std)
+            for std in [0.5, 1., 1.5, 2., 2.5, 3.]
+        ]), p=0.5)
+        dataset = PostTransformDataset(
+            baseset, transform=T.Compose([trsf] + comm_trsf)
+        )
+    elif noise_type == 'replace':
+        assert gblur_std is not None, 'gblur_std should be a floating number'
+        trsf = T.GaussianBlur(math.ceil(4*gblur_std)//2*2+1, sigma=gblur_std)
+        dataset = PostTransformDataset(
+            baseset, transform=T.Compose([trsf] + comm_trsf)
+        )
+    elif noise_type == 'expand' or noise_type == 'append':
+        incset = [PostTransformDataset(
+            baseset, transform=T.Compose(comm_trsf)
+        )] if noise_type == 'append' else []
+        for std in [0.5, 1., 1.5, 2., 2.5, 3.]:
+            trsf = T.GaussianBlur(math.ceil(4*std)//2*2+1, sigma=std)
+            incset.append(PostTransformDataset(
+                baseset, transform=T.Compose([trsf] + comm_trsf)
+            ))
+        dataset = torch.utils.data.ConcatDataset(incset)
     else:
-        raise ValueError('Invalid dataset value')
+        raise ValueError('Invalid noise_type parameter')
 
-    common_transformers = [
-        T.ToTensor(),
-        T.Normalize(mean, std)
-    ]
-    target_transform = None if target_trsf is False else MaskNoiseLabel()
+    return dataset
 
+
+@cache_object(filename='dataset_mean_std.pkl')
+def compute_mean_std(opt, dataset_name):
+    entry = eval(f'torchvision.datasets.{dataset_name}')
+    dataset = entry(root=opt.data_dir, train=True, download=False, transform=T.ToTensor())
+    loader = torch.utils.data.DataLoader(
+        dataset, batch_size=opt.batch_size, shuffle=False, num_workers=2
+    )
+    mean, std = 0., 0.
+    nb_samples = 0.
+    for data, _ in loader:
+        batch_samples = data.size(0)
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+
+    mean /= nb_samples
+    std /= nb_samples
+
+    return mean, std
+
+
+def load_dataset(opt, split, noise=False, **kwargs):
+    entry = eval(f'torchvision.datasets.{opt.dataset.upper()}')
+
+    # handle split
     if split == 'test':
-        base_dataset = cifar(root=opt.data_dir, train=False, download=True)
+        base_dataset = entry(root=opt.data_dir, train=False, download=True)
     elif split == 'val':
-        base_largeset = cifar(root=opt.data_dir, train=True, download=True)
+        base_largeset = entry(root=opt.data_dir, train=True, download=True)
         _, base_dataset = train_test_split(
             base_largeset, test_size=1./50, random_state=2021, stratify=base_largeset.targets)
     elif split == 'train':
-        base_largeset = cifar(root=opt.data_dir, train=True, download=True)
+        base_largeset = entry(root=opt.data_dir, train=True, download=True)
         base_dataset, _ = train_test_split(
             base_largeset, test_size=1./50, random_state=2021, stratify=base_largeset.targets)
     else:
         raise ValueError('Invalid parameter of split')
 
+    # handle noise
+    mean, std = compute_mean_std(opt, opt.dataset.upper())
+    common_transformers = [T.ToTensor(), T.Normalize(mean, std)]
     if noise is True:
-        if noise_type == 'random':
-            trsf = RandomApply(RandomApplyOne([
-                T.GaussianBlur(math.ceil(4*std)//2*2+1, sigma=std)
-                for std in [0.5, 1., 1.5, 2., 2.5, 3.]
-            ]), p=0.5)
-            dataset = PostTransformDataset(
-                base_dataset,
-                transform=T.Compose([trsf] + common_transformers),
-                target_transform=target_transform
-            )
-        elif noise_type == 'replace':
-            assert gblur_std is not None, 'gblur_std should be a floating number'
-            trsf = T.GaussianBlur(math.ceil(4*gblur_std)//2*2+1, sigma=gblur_std)
-            if target_transform is not None:
-                target_transform.set_persistent(1)
-            dataset = PostTransformDataset(
-                base_dataset,
-                transform=T.Compose([trsf] + common_transformers),
-                target_transform=target_transform
-            )
-        elif noise_type == 'expand' or noise_type == 'append':
-            if target_transform is not None:
-                target_transform.set_persistent(0)
-            incset = [PostTransformDataset(
-                base_dataset,
-                transform=T.Compose(common_transformers),
-                target_transform=target_transform
-            )] if noise_type == 'append' else []
-            if target_transform is not None:
-                target_transform.set_persistent(1)
-            for std in [0.5, 1., 1.5, 2., 2.5, 3.]:
-                trsf = T.GaussianBlur(math.ceil(4*std)//2*2+1, sigma=std)
-                incset.append(PostTransformDataset(
-                    base_dataset,
-                    transform=T.Compose([trsf] + common_transformers),
-                    target_transform=target_transform
-                ))
-            dataset = torch.utils.data.ConcatDataset(incset)
-        else:
-            raise ValueError('Invalid noise_type parameter')
+        dataset = load_noisy_dataset(opt, base_dataset, common_transformers, **kwargs)
     else:
         dataset = PostTransformDataset(
-            base_dataset,
-            transform=T.Compose(common_transformers)
+            base_dataset, transform=T.Compose(common_transformers)
         )
 
+    # handle data loader
     shuffle = True if split == 'train' else False
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=opt.batch_size, shuffle=shuffle, num_workers=2
